548be161fe67371e4f8a45b109e6758c798cee8c
proffix4 <proffix4@gmail.com>
2022-09-11 13:56:17 +0600

    update 11/09/2022

--- TSN_DEMO_02_Quadratic/src/main/java/kz/talipovsn/quadratic/MainActivity.java
+++ TSN_DEMO_02_Quadratic/src/main/java/kz/talipovsn/quadratic/MainActivity.java
@@ -1,7 +1,7 @@
 package kz.talipovsn.quadratic;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
